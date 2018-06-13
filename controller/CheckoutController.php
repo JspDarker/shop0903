25c31e83fdce85e3a816fc6fb5295bc039beb809
Huong <admin@Apples-MacBook-Pro.local>
2018-06-13 20:13:51 +0700

    token

--- controller/CheckoutController.php
+++ controller/CheckoutController.php
@@ -1,5 +1,8 @@
 <?php
 include_once "Controller.php";
+include_once 'model/CheckoutModel.php';
+include_once 'helper/Cart.php';
+session_start();
 
 class CheckoutController extends Controller{
 
@@ -14,6 +17,33 @@ class CheckoutController extends Controller{
         $address = $_POST['address'];
         $note = $_POST['note'];
         
+        $model = new CheckoutModel();
+        $idCustomer = $model->saveCustomer($name, $email, $address, $phone);
+        if($idCustomer){
+            //save bill
+            $cart = isset($_SESSION['cart']) ? $_SESSION['cart'] : null;
+            if($cart==null){
+                header('location:index.php'); 
+                return;
+            }
+            // print_r($cart);
+            // die;
+            $dateOrder = date('Y-m-d',time());
+            $promtPrice = $cart->promtPrice;
+            $total = $cart->totalPrice;
+
+            $token = "";
+            $tokenDate = date('Y-m-d H:i:s',time());
+
+            $idBill = $model->saveBill($idCustomer, $dateOrder,$promtPrice, $total, $note,$token, $tokenDate);
+
+
+        }
+        else{
+            $_SESSION['error'] = "Có lỗi xảy ra, vui lòng thử lại";
+            header('location:checkout.php');
+            return;
+        }
     }
 }
 
--- model/CheckoutModel.php
+++ model/CheckoutModel.php
@@ -6,19 +6,22 @@ class CheckoutModel extends DBConnect{
         $sql = "INSERT INTO customers(name,email,address,phone)
                 VALUES ('$name','$email','$address','$phone')";
         $result = $this->executeQuery($sql);
-        return $result ? getLastId() : false;
+        return $result ? $this->getLastId() : false;
     }
 
-    function saveBill($idCustomer, $dateOrder, $total, $note,$token, $tokenDate){
+    function saveBill($idCustomer, $dateOrder, $promtPrice, $total, $note,$token, $tokenDate){
 
-        $sql = "INSERT INTO bills(id_customer,date_order,total, note, token, token_date)
-                VALUES ($idCustomer, '$dateOrder', $total, '$note','$token', '$tokenDate')";
+        $sql = "INSERT INTO bills(id_customer,date_order, promt_price, total, note, token, token_date)
+                VALUES ($idCustomer, '$dateOrder', $promtPrice, $total, '$note','$token', '$tokenDate')";
         $result = $this->executeQuery($sql);
-        return $result ? getLastId() : false;
+        return $result ? $this->getLastId() : false;
         
     }
-    function saveBillDetail(){
-        
+
+    function saveBillDetail($idBill,$idProduct, $qty, $price){
+        $sql = "INSERT INTO bill_detail(id_bill,id_product,quantity,price)
+                VALUES($idBill,$idProduct, $qty, $price)";
+        return $this->executeQuery($sql); 
     }
 }
 
